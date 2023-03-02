98304a47e5e27119c77da00e556672ddc613220e
Rony Valdelamar <ronnyandres120@gmail.com>
2023-03-02 10:11:31 -0500

    Cambios en Tabs

--- src/pages/Tabs.jsx
+++ src/pages/Tabs.jsx
@@ -1,14 +1,29 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, Link, useParams } from "react-router-dom";
 import useMediaQuery from "@mui/material/useMediaQuery";
+import { gestionTabsItems } from "../utils/gestionTabsItems";
 
 const Tabs = () => {
   const isNotAPhone = useMediaQuery("(min-width: 1000px)");
+
+ 
   return (
-    isNotAPhone && (
+    <div>
       <NavLink to="/" className="title">
-        pharma.pa
+        {isNotAPhone && <div class="px-6">pharma.pa</div>}
       </NavLink>
-    )
+    
+      <div class="text-sm font-medium text-center text-gray-500 border-b border-gray-200 ">
+        <ul class="flex flex-wrap -mb-px">
+        <li class="mr-2">
+          {gestionTabsItems.map((item)=><Link 
+              to="#"
+              className="inline-block p-4 hover:border-b-2 border-transparent hover:border-black px-10"
+            >{item}
+            </Link>)}
+        </li>
+        </ul>
+      </div>
+    </div>
   );
 };
 
---
+++ src/utils/gestionTabsItems.js
@@ -0,0 +1,3 @@
+export const gestionTabsItems = [
+    "Medicamentos","Personas","Bodega","Secciones","Correos"
+]
